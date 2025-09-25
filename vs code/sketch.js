4fa7e6d5f3490fa3fe22b4f88a37022fb7d39f48
4701256-GH <4701256@spsd.sk.ca>
2025-09-25 09:23:02 -0600

    add interactive screen

---
+++ vs code/sketch.js
@@ -0,0 +1,18 @@
+// INteractive Screen
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+
+  circle(mouseX, mouseY, 100);
+
+}
