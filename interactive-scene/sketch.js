e4fbc864a1d577408956ab8d2d915265cd87d93b
4701256-GH <4701256@spsd.sk.ca>
2025-09-29 09:13:10 -0600

    Update sketch.js

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -6,12 +6,52 @@
 // - describe what you did to take this project "above and beyond"
 
 
+let lines = 5;
+let y = 0;
+let lineToggle = false;
+let strokeColor;
+let strokeSize = 2;
+let backGround = "white";
+let rainbow = "on";
+let r;
+let g;
+let b;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
-
-  circle(mouseX, mouseY, 100);
+  if(rainbow === "on"){
+    r = mouseX/2;
+    g = (mouseX - mouseY) * 4 ; 
+    b = mouseY;
+    stroke(r, g, b);
+  }
+  
+  background(backGround);
+  
+  let x = 0;
+  
+  y = 0;
+  while(x < 401) {
+    line(x, y, mouseX, mouseY);
+    x = x + lines;
+  }
+  x = 0;
+  while(x < 401) {
+    line(x, 400, mouseX, mouseY);
+    x = x + lines;
+  } 
+  y = 0;
+  while(y < 401) {
+    line(x, y, mouseX, mouseY);
+    y = y + lines;
+  }
+  y = 0;
+  x = 0;
+  while(y < 401) {
+    line(x, y, mouseX, mouseY);
+    y = y + lines;
+  }
 }
