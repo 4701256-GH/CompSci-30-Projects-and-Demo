2e24466c143ded664a8d4786b1e392ebf777ad18
4701256-GH <4701256@spsd.sk.ca>
2025-10-02 09:52:02 -0600

    add mouse wheel controls

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -6,7 +6,7 @@
 // - describe what you did to take this project "above and beyond"
 
 
-let lineSpacing = 2;
+let lineSpacing = 15;
 let y = 0;
 let lineToggle = false;
 let strokeColor = "black";
@@ -18,10 +18,13 @@ let g;
 let b;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  let cnv = createCanvas(windowWidth, windowHeight);
+  cnv.mouseWheel(changeLineSpacing);
 }
 
+
 function draw() {
+
   if(rainbow === "on"){
     r = mouseX/6;
     g = (mouseX - mouseY) / 6 ; 
@@ -41,21 +44,44 @@ function draw() {
     line(x, y, mouseX, mouseY);
     x = x + lineSpacing;
   }
+ 
   x = 0;
   y = windowHeight;
-  while(x < windowWidth) {
+  while(x <= windowWidth ) {
     line(x, y, mouseX, mouseY);
     x = x + lineSpacing;
   } 
+
+
+  x = width; 
   y = 0;
   while(y < windowHeight) {
     line(x, y, mouseX, mouseY);
     y = y + lineSpacing;
   }
+
+
   y = 0;
   x = 0;
   while(y < windowHeight) {
     line(x, y, mouseX, mouseY);
     y = y + lineSpacing;
   }
+
+  if (lines > 1 && event.deltaY > 0) {
+    lines = lines - 1; 
+  }
+  else if (lines < 50 && event.deltaY < 0) {
+    lines = lines + 1; 
+ }
+
 }
+
+// function changeLineSpacing() {
+//   if (lines > 1 && event.deltaY > 0) {
+//     lines = lines - 1; 
+//   }
+//   else if (lines < 50 && event.deltaY < 0) {
+//     lines = lines + 1; 
+//  }
+
