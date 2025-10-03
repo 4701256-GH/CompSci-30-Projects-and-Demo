f9078d40faaffc23ba6242bf7a52c860b26a7cb5
4701256-GH <4701256@spsd.sk.ca>
2025-10-03 09:53:52 -0600

    finish

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -6,45 +6,49 @@
 // - describe what you did to take this project "above and beyond"
 
 
-let lineSpacing = 15;
-let y = 0;
-let lineToggle = false;
-let strokeColor = "black";
-let strokeSize = 2;
-let backGround = "white";
+let lineSpacing = 5;
+let strokeColor = "white"; 
+let backGround = "black";
 let rainbow = "off";
+let x = 0;
+let y = 0;
 let r;
 let g;
 let b;
 
+
 function setup() {
-  let cnv = createCanvas(windowWidth, windowHeight);
+ let cnv = createCanvas(windowWidth, windowHeight);
   cnv.mouseWheel(changeLineSpacing);
 }
 
 
 function draw() {
 
+  changerainbow();
+  
+// set rainbow colors based on the mouse coordinates
   if(rainbow === "on"){
     r = mouseX/6;
     g = (mouseX - mouseY) / 6 ; 
     b = mouseY/3;
     stroke(r, g, b);
   }
+// if no rainbow use set stroke color
   else{
-    srtoke = strokeColor;
+    stroke(strokeColor);
   }
   
   background(backGround);
-  
-  let x = 0;
-  
+   
+// Top part of the screen
   y = 0;
   while(x < windowWidth) {
     line(x, y, mouseX, mouseY);
     x = x + lineSpacing;
   }
  
+// bottom of the screen  
   x = 0;
   y = windowHeight;
   while(x <= windowWidth ) {
@@ -52,7 +56,7 @@ function draw() {
     x = x + lineSpacing;
   } 
 
-
+// right of the screen
   x = width; 
   y = 0;
   while(y < windowHeight) {
@@ -60,28 +64,29 @@ function draw() {
     y = y + lineSpacing;
   }
 
-
+// left of the screen
   y = 0;
   x = 0;
   while(y < windowHeight) {
     line(x, y, mouseX, mouseY);
     y = y + lineSpacing;
   }
-
-  if (lines > 1 && event.deltaY > 0) {
-    lines = lines - 1; 
-  }
-  else if (lines < 50 && event.deltaY < 0) {
-    lines = lines + 1; 
- }
-
 }
 
-// function changeLineSpacing() {
-//   if (lines > 1 && event.deltaY > 0) {
-//     lines = lines - 1; 
-//   }
-//   else if (lines < 50 && event.deltaY < 0) {
-//     lines = lines + 1; 
-//  }
+// uses scroll wheel to increase and decrease amount of linespacing
+function changeLineSpacing() {
+  if (lineSpacing > 1 && event.deltaY > 0) {
+    lineSpacing = lineSpacing - 1; 
+  }
+  else if (lineSpacing < 50 && event.deltaY < 0) {
+    lineSpacing = lineSpacing + 1; 
+ }
+}
 
+// click R or T to turn on and off the rainbow mode
+function changerainbow(){
+  if (keyIsDown(82) === true && rainbow === "off")
+    rainbow = "on";
+  else if (keyIsDown(84) === true && rainbow === "on")
+    rainbow = "off";
+}
