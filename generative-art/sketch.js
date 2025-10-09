6972bd19a17b2031ee9dd0e34827a38d3db30bbb
4701256-GH <4701256@spsd.sk.ca>
2025-10-09 09:50:50 -0600

    aray practise

---
+++ generative-art/sketch.js
@@ -0,0 +1,47 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let tilesize = 50;
+let someTile;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  for (let x = 0; x < width; x += tilesize)
+    someTile = spawnTile(x, height/2);
+
+
+}
+
+function draw() {
+  background(220);
+
+  for (let )
+  line(someTile.x1, someTile.y1, someTile.x2, someTile.x2);
+}
+
+function spawnTile(x,y) {
+  let chose = ranmdom(100);
+  let tile;
+
+  if (choice > 50){
+    tile = {
+      x1: x - tilesize/2,
+      y1: y + tilesize/2,
+      x2: x + tilesize/2,
+      y2: y + tilesize/2,
+    };
+
+  }
+  else{
+    tile = {
+      x1: x - tilesize/2,
+      y1: y - tilesize/2,
+      x2: x + tilesize/2,
+      y2: y + tilesize/2,
+    };
+  }
+}  
--- index.md
+++ index.md
@@ -3,6 +3,7 @@
 ## Demos
 - [Traffic Light Simulator](traffic-lights)
 - [Bouncing Balls](bouncing-balls)
+- [Generatve Art](generative-art) 
 
 ## Assignments
-- [Interactive Scene](interactive-scene)
+- [Interactive Scene](interactive-scene) 
