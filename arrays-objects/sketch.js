2372e804177c1908efa10c54b7ab8d6fb789ee81
4701256-GH <4701256@spsd.sk.ca>
2025-10-10 09:24:05 -0600

    sterted code

---
+++ arrays-objects/sketch.js
@@ -0,0 +1,19 @@
+// Audio Visualizer
+// Trey Styles
+// Oct 10
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
+
+function drawCircleThing() {
+  
+}
