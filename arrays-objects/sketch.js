0d0bc69044cc029239f06bb238b8ad23e35115a0
4701256-GH <4701256@spsd.sk.ca>
2025-10-14 09:53:03 -0600

    stating the drawing

--- arrays-objects/sketch.js
+++ arrays-objects/sketch.js
@@ -5,15 +5,25 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let angle;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  background("black");
 }
 
 function drawCircleThing() {
-  
+  stroke("white");
+for (i = 0; i < 500; i++) {
+  let angle = i * 0.5;
+  let radius = i * 0.4;
+  let x = radius * cos(angle + time) + 200;
+  let y = radius * sin(angle + time) + 200;
+
+  point(x,y)
+
+}
 }
